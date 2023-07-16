54da63ad1dd9fa0147740b60c0391589e1720389
carolinecharr <101280813+carolinecharr@users.noreply.github.com>
2023-07-16 09:50:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-from streamlit_aggrid import AgGrid, GridOptionsBuilder, AgGridConfigBuilder
 
 # Load the leads data from the CSV file
 leads_data = pd.read_csv('Leads.csv')
@@ -33,26 +32,12 @@ if filter_location:
 if filter_stage != 'All':
     filtered_leads = filtered_leads[filtered_leads['Stage'].astype(str) == filter_stage]
 
-# Configure the editable table using streamlit-aggrid
-gb = GridOptionsBuilder.from_dataframe(filtered_leads)
-gb.configure_default_column(groupable=True, editable=True)
-grid_options = gb.build()
+# Remove the index column from the displayed table
+columns_to_display = ['First Name', 'Last Name', 'Job Title', 'Location', 'Stage']
+filtered_leads = filtered_leads[columns_to_display].reset_index(drop=True)
 
-# Create a copy of the filtered leads for display and updating
-updated_leads = filtered_leads.copy()
-
-# Display the editable table
-ag = AgGrid(
-    updated_leads,
-    gridOptions=grid_options,
-    width='100%',
-    data_return_mode='AS_INPUT',
-    update_mode='VALUE_CHANGED',
-    fit_columns_on_grid_load=True,
-)
-
-# Update the leads_data with the updated stage values
-leads_data.update(updated_leads)
+# Create a table to display the filtered leads
+st.table(filtered_leads)
 
 # Save the updated leads data to the CSV file (optional)
 # leads_data.to_csv('Leads.csv', index=False)
