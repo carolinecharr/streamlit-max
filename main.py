b51a3e725672ff8f55e3da4b61a8f418820b490d
carolinecharr <101280813+carolinecharr@users.noreply.github.com>
2023-07-16 09:41:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 leads_data = pd.read_csv('Leads.csv')
 
 # Set page title
-st.title('Collecting Leads gg')
+st.title('Collecting Leads from LinkedIn')
 
 # Define the filter options for Name, Location, and Stage
 col1, col2, col3 = st.columns(3)
@@ -21,44 +21,20 @@ stage_options = ['All', '1', '2', '3']
 filter_stage = col3.selectbox('Filter by Stage', stage_options)
 
 # Apply filters to the leads data
-filtered_leads = leads_data[
-    (leads_data['First Name'].str.contains(filter_name, case=False)) &
-    (leads_data['Location'].str.contains(filter_location, case=False))
-]
+filtered_leads = leads_data.copy()
+
+if filter_name:
+    filtered_leads = filtered_leads[filtered_leads['First Name'].str.contains(filter_name, case=False)]
+
+if filter_location:
+    filtered_leads = filtered_leads[filtered_leads['Location'].str.contains(filter_location, case=False)]
 
 if filter_stage != 'All':
     filtered_leads = filtered_leads[filtered_leads['Stage'].astype(str) == filter_stage]
 
-# Set default value of 1 for leads with an empty stage
-filtered_leads.loc[filtered_leads['Stage'].isnull(), 'Stage'] = 1
-
-# Create a copy of the filtered leads for display and updating
-updated_leads = filtered_leads.copy()
-
-# Create an editable table for the leads
+# Create a table to display the filtered leads
 columns_to_display = ['First Name', 'Last Name', 'Job Title', 'Location', 'Stage']
+st.table(filtered_leads[columns_to_display])
 
-# Helper function to update stage value on cell update
-def update_stage_value(updated_stage, row_index, col_index):
-    updated_leads.at[row_index, columns_to_display[col_index]] = updated_stage
-
-# Display the table with editable stage values
-for idx, row in updated_leads.iterrows():
-    for col_idx, column in enumerate(columns_to_display):
-        if column == 'Stage':
-            if filter_stage == 'All':
-                st.write(row[column])
-            else:
-                updated_stage = st.selectbox(f"Lead {idx + 1} Stage", stage_options, index=row[column] - 1)
-                if updated_stage != 'All':
-                    update_stage_value(int(updated_stage), idx, col_idx)
-        else:
-            st.write(row[column])
-
-st.table(updated_leads[columns_to_display])
-
-# Update the leads_data with the updated stage values
-leads_data.update(updated_leads)
-
-# Save the updated leads data to the CSV file
-leads_data.to_csv('Leads.csv', index=False)
+# Save the updated leads data to the CSV file (optional)
+# leads_data.to_csv('Leads.csv', index=False)
