b8576b6193195982dbcbc86ee3b7846accb28b18
carolinecharr <101280813+carolinecharr@users.noreply.github.com>
2023-07-16 09:07:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,24 +5,31 @@ import pandas as pd
 leads_data = pd.read_csv('Leads.csv')
 
 # Define the filter options for Name, Location, and Stage
-filter_name = st.sidebar.text_input('Filter by Name')
-filter_location = st.sidebar.text_input('Filter by Location')
-filter_stage = st.sidebar.selectbox('Filter by Stage', ['1', '2', '3'])
+filter_name = st.text_input('Filter by Name')
+filter_location = st.text_input('Filter by Location')
+filter_stage = st.selectbox('Filter by Stage', ['', '1', '2', '3'])
 
 # Apply filters to the leads data
 filtered_leads = leads_data[
     (leads_data['First Name'].str.contains(filter_name, case=False)) &
     (leads_data['Location'].str.contains(filter_location, case=False)) &
-    (leads_data['Stage'] == int(filter_stage))
+    (leads_data['Stage'].astype(str) == filter_stage)
 ]
 
-# Display the filtered leads table
-st.table(filtered_leads)
+# Display the filtered leads table with editable stage values
+columns_to_display = ['First Name', 'Last Name', 'Job Title', 'Location', 'Stage']
+updated_leads = filtered_leads[columns_to_display].copy()
 
-# Allow users to update the stage value for each lead using a dropdown button
-if st.checkbox('Update Stage Value'):
-    updated_leads = leads_data.copy()
-    for idx, row in updated_leads.iterrows():
-        new_stage = st.selectbox(f"Lead {idx + 1} Stage", ['1', '2', '3'], index=row['Stage'] - 1)
-        updated_leads.at[idx, 'Stage'] = int(new_stage)
-    st.table(updated_leads)
+for idx, row in updated_leads.iterrows():
+    new_stage = st.selectbox(f"Lead {idx + 1} Stage", ['', '1', '2', '3'], index=row['Stage'])
+    updated_leads.at[idx, 'Stage'] = int(new_stage) if new_stage != '' else None
+
+st.table(updated_leads.style.set_table_styles([
+    {'selector': 'th', 'props': [('max-width', '150px')]}  # Set maximum width for all columns
+]))
+
+# Update the leads_data with the updated stage values
+leads_data.update(updated_leads)
+
+# Save the updated leads data to the CSV file
+leads_data.to_csv('leads.csv', index=False)
