1f8d6c00a54820c7cb80638cfec771a4db483e76
carolinecharr <101280813+carolinecharr@users.noreply.github.com>
2023-07-16 09:33:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 leads_data = pd.read_csv('Leads.csv')
 
 # Set page title
-st.title('Collecting Leads')
+st.title('Collecting Leads gg')
 
 # Define the filter options for Name, Location, and Stage
 col1, col2, col3 = st.columns(3)
@@ -17,7 +17,7 @@ filter_name = col1.text_input('Filter by Name')
 filter_location = col2.text_input('Filter by Location')
 
 # Selectbox for Stage filter
-stage_options = ['', '1', '2', '3']
+stage_options = ['All', '1', '2', '3']
 filter_stage = col3.selectbox('Filter by Stage', stage_options)
 
 # Apply filters to the leads data
@@ -26,7 +26,7 @@ filtered_leads = leads_data[
     (leads_data['Location'].str.contains(filter_location, case=False))
 ]
 
-if filter_stage != '':
+if filter_stage != 'All':
     filtered_leads = filtered_leads[filtered_leads['Stage'].astype(str) == filter_stage]
 
 # Set default value of 1 for leads with an empty stage
@@ -46,11 +46,11 @@ def update_stage_value(updated_stage, row_index, col_index):
 for idx, row in updated_leads.iterrows():
     for col_idx, column in enumerate(columns_to_display):
         if column == 'Stage':
-            if filter_stage == '':
+            if filter_stage == 'All':
                 st.write(row[column])
             else:
-                updated_stage = st.selectbox(f"Lead {idx + 1} Stage", ['', '1', '2', '3'], index=row[column] - 1)
-                if updated_stage != '':
+                updated_stage = st.selectbox(f"Lead {idx + 1} Stage", stage_options, index=row[column] - 1)
+                if updated_stage != 'All':
                     update_stage_value(int(updated_stage), idx, col_idx)
         else:
             st.write(row[column])
