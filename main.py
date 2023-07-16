4f54248379dfd142247164f706338ea73c860db2
carolinecharr <101280813+carolinecharr@users.noreply.github.com>
2023-07-16 09:51:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -32,12 +32,29 @@ if filter_location:
 if filter_stage != 'All':
     filtered_leads = filtered_leads[filtered_leads['Stage'].astype(str) == filter_stage]
 
-# Remove the index column from the displayed table
-columns_to_display = ['First Name', 'Last Name', 'Job Title', 'Location', 'Stage']
-filtered_leads = filtered_leads[columns_to_display].reset_index(drop=True)
+# Create a copy of the filtered leads for display and updating
+updated_leads = filtered_leads.copy()
 
-# Create a table to display the filtered leads
-st.table(filtered_leads)
+# Create an editable table for the leads
+columns_to_display = ['First Name', 'Last Name', 'Job Title', 'Location', 'Stage']
+
+# Helper function to update stage value on cell update
+def update_stage_value(updated_stage, row_index):
+    updated_leads.at[row_index, 'Stage'] = updated_stage
+
+# Display the table with editable stage values
+for idx, row in updated_leads.iterrows():
+    for column in columns_to_display:
+        if column == 'Stage':
+            if filter_stage == 'All':
+                st.write(row[column])
+            else:
+                updated_stage = st.selectbox(f"Lead {idx + 1} Stage", ['', '1', '2', '3'], index=row[column] - 1)
+                if updated_stage != '':
+                    update_stage_value(int(updated_stage), idx)
+        else:
+            st.write(row[column])
 
 # Save the updated leads data to the CSV file (optional)
+# leads_data.update(updated_leads)
 # leads_data.to_csv('Leads.csv', index=False)
