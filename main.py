7ab7bc83890ecb09882e33955ff632dead39716f
carolinecharr <101280813+carolinecharr@users.noreply.github.com>
2023-07-16 09:27:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,29 +5,29 @@ import pandas as pd
 leads_data = pd.read_csv('Leads.csv')
 
 # Set page title
-st.title('Collecting Leads from LinkedIn')
+st.title('Collecting Leads from LinkedIn 1')
 
 # Define the filter options for Name, Location, and Stage
-filter_name, filter_location, filter_stage = st.beta_columns(3)
+col1, col2, col3 = st.beta_columns(3)
 
 # Text input for Name filter
-name_input = filter_name.text_input('Filter by Name')
+filter_name = col1.text_input('Filter by Name')
 
 # Text input for Location filter
-location_input = filter_location.text_input('Filter by Location')
+filter_location = col2.text_input('Filter by Location')
 
 # Selectbox for Stage filter
 stage_options = ['', '1', '2', '3']
-stage_input = filter_stage.selectbox('Filter by Stage', stage_options)
+filter_stage = col3.selectbox('Filter by Stage', stage_options)
 
 # Apply filters to the leads data
 filtered_leads = leads_data[
-    (leads_data['First Name'].str.contains(name_input, case=False)) &
-    (leads_data['Location'].str.contains(location_input, case=False))
+    (leads_data['First Name'].str.contains(filter_name, case=False)) &
+    (leads_data['Location'].str.contains(filter_location, case=False))
 ]
 
-if stage_input != '':
-    filtered_leads = filtered_leads[filtered_leads['Stage'].astype(str) == stage_input]
+if filter_stage != '':
+    filtered_leads = filtered_leads[filtered_leads['Stage'].astype(str) == filter_stage]
 
 # Set default value of 1 for leads with an empty stage
 filtered_leads.loc[filtered_leads['Stage'].isnull(), 'Stage'] = 1
@@ -46,7 +46,7 @@ def update_stage_value(updated_stage, row_index, col_index):
 for idx, row in updated_leads.iterrows():
     for col_idx, column in enumerate(columns_to_display):
         if column == 'Stage':
-            if stage_input == '':
+            if filter_stage == '':
                 st.write(row[column])
             else:
                 updated_stage = st.selectbox(f"Lead {idx + 1} Stage", ['', '1', '2', '3'], index=row[column] - 1)
