9ec2b588bab2317f682ad499858278c0adb2fb32
carolinecharr <101280813+carolinecharr@users.noreply.github.com>
2023-07-16 09:55:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -32,29 +32,28 @@ if filter_location:
 if filter_stage != 'All':
     filtered_leads = filtered_leads[filtered_leads['Stage'].astype(str) == filter_stage]
 
-# Create a copy of the filtered leads for display and updating
-updated_leads = filtered_leads.copy()
-
 # Create an editable table for the leads
-columns_to_display = updated_leads.columns.tolist()
+columns_to_display = filtered_leads.columns.tolist()
 
 # Helper function to update stage value on cell update
 def update_stage_value(updated_stage, row_index):
-    updated_leads.at[row_index, 'Stage'] = updated_stage
+    filtered_leads.at[row_index, 'Stage'] = updated_stage
 
 # Display the table with editable stage values
-for idx, row in updated_leads.iterrows():
-    for column in columns_to_display:
-        if column == 'Stage':
-            if filter_stage == 'All':
-                st.write(row[column])
+with st.form(key='lead_form'):
+    for idx, row in filtered_leads.iterrows():
+        for column in columns_to_display:
+            if column == 'Stage':
+                if filter_stage == 'All':
+                    st.write(row[column])
+                else:
+                    updated_stage = st.selectbox(f"Lead {idx + 1} Stage", ['', '1', '2', '3'], index=row[column] - 1)
+                    if updated_stage != '':
+                        update_stage_value(int(updated_stage), idx)
             else:
-                updated_stage = st.selectbox(f"Lead {idx + 1} Stage", ['', '1', '2', '3'], index=row[column] - 1)
-                if updated_stage != '':
-                    update_stage_value(int(updated_stage), idx)
-        else:
-            st.write(row[column])
+                st.write(row[column])
+    st.form_submit_button(label='Save')
 
 # Save the updated leads data to the CSV file (optional)
-# leads_data.update(updated_leads)
+# leads_data.update(filtered_leads)
 # leads_data.to_csv('Leads.csv', index=False)
