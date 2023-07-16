d7753825dbafedf3ad51fa3fae664a30b5f29fe3
carolinecharr <101280813+carolinecharr@users.noreply.github.com>
2023-07-16 09:03:16 +0200

    Filtered Leads

--- main.py
+++ main.py
@@ -1,57 +1,28 @@
 import streamlit as st
-from bs4 import BeautifulSoup
-import requests
 import pandas as pd
 
-# Define the LinkedIn search URL and keywords
-search_url = "https://www.linkedin.com/search/results/people/?keywords={}"
-keywords = "your_keywords_here"
+# Load the leads data from the CSV file
+leads_data = pd.read_csv('leads.csv')
 
-# Scrape LinkedIn search results
-def scrape_linkedin(search_url, keywords):
-    leads = []
+# Define the filter options for Name, Location, and Stage
+filter_name = st.sidebar.text_input('Filter by Name')
+filter_location = st.sidebar.text_input('Filter by Location')
+filter_stage = st.sidebar.selectbox('Filter by Stage', ['1', '2', '3'])
 
-    # Create the search URL with keywords
-    url = search_url.format(keywords)
+# Apply filters to the leads data
+filtered_leads = leads_data[
+    (leads_data['First Name'].str.contains(filter_name, case=False)) &
+    (leads_data['Location'].str.contains(filter_location, case=False)) &
+    (leads_data['Stage'] == int(filter_stage))
+]
 
-    # Send GET request to the URL
-    response = requests.get(url)
+# Display the filtered leads table
+st.table(filtered_leads)
 
-    # Parse the HTML content using BeautifulSoup
-    soup = BeautifulSoup(response.content, "html.parser")
-
-    # Extract lead information
-    results = soup.find_all("li", {"class": "search-result"})
-    for result in results:
-        lead = {}
-        lead["Name"] = result.find("span", {"class": "name actor-name"}).text.strip()
-        lead["LinkedProfile"] = "https://www.linkedin.com" + result.find("a", href=True)["href"]
-        lead["LinkedIn Title"] = result.find("p", {"class": "subline-level-1 t-14 t-black t-normal search-result__truncate"}).text.strip()
-        lead["Location"] = result.find("p", {"class": "subline-level-2 t-12 t-black--light t-normal search-result__truncate"}).text.strip()
-        lead["Company"] = result.find("p", {"class": "subline-level-2 t-12 t-black--light t-normal search-result__truncate"}).find_next_sibling("p").text.strip()
-        lead["email"] = ""  # Add your email extraction logic here
-        leads.append(lead)
-
-    return leads
-
-# Streamlit app
-def main():
-    # Page title
-    st.title("LinkedIn Lead Collector")
-
-    # Collect leads
-    leads = scrape_linkedin(search_url, keywords)
-
-    # Create a DataFrame from the leads
-    leads_df = pd.DataFrame(leads)
-
-    # Add a column for user input (stage)
-    stages = ["Not Started", "In Progress", "Done", "Cancelled"]
-    leads_df["Stage"] = st.sidebar.selectbox("Select Stage", stages)
-
-    # Display leads table
-    st.header("Leads Table")
-    st.dataframe(leads_df)
-
-if __name__ == "__main__":
-    main()
+# Allow users to update the stage value for each lead using a dropdown button
+if st.checkbox('Update Stage Value'):
+    updated_leads = leads_data.copy()
+    for idx, row in updated_leads.iterrows():
+        new_stage = st.selectbox(f"Lead {idx + 1} Stage", ['1', '2', '3'], index=row['Stage'] - 1)
+        updated_leads.at[idx, 'Stage'] = int(new_stage)
+    st.table(updated_leads)
